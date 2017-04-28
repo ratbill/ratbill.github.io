640b98c0c8205adac966039401d180d8dad99e54
Joao Gracinha <jgcmarins@gmail.com>
2017-04-28 20:05:07 -0300

    first commit

---
+++ assets/js/functions.js
@@ -0,0 +1,16 @@
+jQuery(function($) {
+	
+	/* Back to top button */
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+			$('.scrollup').fadeIn();
+			} else {
+				$('.scrollup').fadeOut();
+		}
+	});
+	$('.scrollup').click(function() {
+		$("html, body").animate({ scrollTop: 0 }, 1000);
+		return false;
+	});
+
+});
